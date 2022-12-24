48783ce699f070d007a6a89d797ae765adb7e3f7
Yuri-Vanderon <garanovich2@gmail.com>
2022-12-24 19:31:34 +0300

    points: 1, 2.

--- src/Main.java
+++ src/Main.java
@@ -7,20 +7,43 @@ public class Main {
 
         Scanner scanner = new Scanner(System.in);
 
-         String snum = scanner.next();
+        String snum = scanner.next();
 
          StringBuffer rev = new StringBuffer(snum);
 
          rev.reverse();
 
-         int num = Integer.parseInt(String.valueOf(rev));
 
-        System.out.println("Result: " + rev);
+        System.out.println("Reversed number: "+rev);
 
 
-         }
+        int i;
+
+
+        for (i = 0; i < 10; i++) {
+
+
+            if (snum.indexOf("" + i) > -1) {
+
+                    System.out.println("Min number: "+i);
+                    break;
+
+            }
+        }
+
+        for (i = 9; i >= 0; i--) {
+
+            if (snum.indexOf("" + i) > -1) {
+
+                System.out.println("Max number: "+i);
+                break;
+            }
+
 
         }
+
+    }
+}
 
 
 
